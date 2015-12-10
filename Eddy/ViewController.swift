69087f89a61a647611932519ff178566afd5e962
afalck <junk.afalck@gmail.com>
2015-12-09 20:19:30 -0800

    Initial Commit

---
+++ Eddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eddy
+//
+//  Created by Arturo Falck on 12/9/15.
+//  Copyright © 2015 Arturo Falck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
